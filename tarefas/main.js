e9d0e433934d6ea7ab776518e67ed1c038744d97
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2024-01-15 14:16:25 -0300

    testes

--- tarefas/main.js
+++ tarefas/main.js
@@ -1,9 +1,7 @@
 const prompt = require('prompt-sync')({ sigint: true });
-
-// Importa a classe TaskManager do arquivo GerenciadorTarefas.js
 const GerenciadorTarefas = require('./GerenciadorTarefas');
 
-// Função para exibir o menu de opções
+
 function exibirMenu() {
   console.log("\nTaskMaster - Gerenciador de Tarefas");
   console.log("1 - Adicionar tarefa");
@@ -12,29 +10,27 @@ function exibirMenu() {
   console.log("0 - Sair");
 }
 
-// Função principal
+
 function main() {
-  // Cria uma instância de TaskManager
+ 
   const taskManager = new (GerenciadorTarefas);
 
   while (true) {
-    // Exibe o menu de opções
+   
     exibirMenu();
 
-    // Obtém a escolha do usuário
     let escolha = parseInt(prompt("Escolha sua opção: "));
 
     switch (escolha) {
       case 0:
         console.log('\nSaindo...Volte sempre, amigo!');
-        return; // Encerra o loop e sai da função main
+        return;
 
       case 1:
-        // Solicita título e descrição da tarefa
+
         const titulo = prompt("Digite o título da tarefa: ");
         const descricao = prompt("Digite a descrição da tarefa: ");
         
-        // Adiciona a tarefa e exibe mensagem de sucesso
         taskManager.adicionarTarefa(titulo, descricao);
         console.log("\nTarefa adicionada com sucesso!");
         break;
@@ -51,9 +47,9 @@ function main() {
           break;
 
       case 3:
-        // Solicita o índice da tarefa a ser marcada como concluída
+    
         const indiceTarefa = parseInt(prompt("Digite o índice da tarefa a ser marcada como concluída: "));
-        taskManager.marcarTarefaConcluida(indiceTarefa - 1); // Marca a tarefa
+        taskManager.marcarTarefaConcluida(indiceTarefa - 1); // Marca a tarefa como concluída
         console.log("\nTarefa marcada como concluída com sucesso!");
         break;
 
